a43316d560af3c5dedf03f5f72ccf96e1a982b1c
silcream <81163573+silcream@users.noreply.github.com>
2022-01-17 02:07:30 +0900

    Add files via upload

---
+++ happycode.py
@@ -0,0 +1,26 @@
+import sys
+
+k = {}
+def w(a, b, c):
+
+    if (a <= 0 or b <= 0 or c <= 0):
+        return 1
+    elif (a > 20 or b > 20 or c > 20):
+        return w(20, 20, 20)
+
+
+    key = '{} {} {}'.format(a, b, c)
+
+    if key in k:
+        return k[key]
+
+    result =  w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    k[key] = result
+
+    return result
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print('w({}, {}, {}) = {}'.format(a, b, c, w(a, b, c)))
